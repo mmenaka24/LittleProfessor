b2a49aaf5e0835a21af55327dcb3995b77373d11
mmenaka24 <menaka_maruthainar@live.com>
2025-03-31 18:37:12 +0100

    fix
    changes main logic to avoid UnboundLocalError

--- professor.py
+++ professor.py
@@ -6,27 +6,22 @@ def main():
 
     score = 0
 
-    for i in range(10):
+    for q in range(10):
         x = generate_integer(level)
         y = generate_integer(level)
         ans = x + y
 
-        correct = False
-
-        for i in range(3):
+        for attempt in range(3):
             try:
                 guess = int(input((f"{x} + {y} = ")))
+                if guess == ans:
+                    score += 1
+                    break
+                else:
+                    raise ValueError
             except ValueError:
-                pass
-
-            if guess == ans:
-                correct = True
-                break
-            else:
                 print("EEE")
-
-        if correct:
-            score += 1
+                
         else:
             print(f"{x} + {y} = {ans}")
 
@@ -48,8 +43,8 @@ def get_level():
 def generate_integer(level):
     if not level in [1, 2, 3]:
         raise ValueError
-    else:
-        return random.randint(0, pow(10, level) - 1)
+    
+    return random.randint(0, pow(10, level) - 1)
 
 
 if __name__ == "__main__":
